7cd37ae25439b47f578aec2fffb20128e2b9b1cb
王博林 <Brycewlinw@163.com>
2020-09-19 23:56:13 +0800

    工厂模式

--- src/Factorymethod/CarFactory.java
+++ src/Factorymethod/CarFactory.java
@@ -1,5 +1,5 @@
 package Factorymethod;
-
+//针对一种产品建一个工厂
 public class CarFactory {
     public Moveable createCar(){
         System.out.println("a car create!");
--- src/Factorymethod/SimpleVehicleFactory.java
+++ src/Factorymethod/SimpleVehicleFactory.java
@@ -1,7 +1,8 @@
 package Factorymethod;
 
 /**
- * 简单工厂的可扩展性并不好
+ * 简单工厂
+ * 可扩展性并不好
  */
 public class SimpleVehicleFactory {
     public Car createCar(){
